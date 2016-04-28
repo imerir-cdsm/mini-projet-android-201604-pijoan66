04900fb64cea9f5a64247348199c75af023a8af4
pijoan <jo66380@gmail.com>
2016-04-28 09:05:28 +0200

    Création d'un Artiste Adapter

--- app/src/main/java/com/example/pijoan/myapplication/TwoFragment.java
+++ app/src/main/java/com/example/pijoan/myapplication/TwoFragment.java
@@ -6,13 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.androidquery.AQuery;
+
+import io.realm.Realm;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TwoFragment extends Fragment {
+    View v;
 
+    Realm realm;
+    ListView ListArtiste;
+
+    ArtisteAdapter myadapter;
+
+    AQuery aq;
 
     public TwoFragment() {
         // Required empty public constructor
@@ -23,7 +35,9 @@ public class TwoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        v =inflater.inflate(R.layout.fragment_two, container, false);
+
+        return v;
     }
 
 }
